e4c8a79fdd943d4d8b735e5ea0679ef8e8cac52f
Lenni <guentherhanslphdyt@gmail.com>
2024-02-22 19:41:34 +0100

    fix link

--- update-viewer/src/components/common/header.tsx
+++ update-viewer/src/components/common/header.tsx
@@ -9,7 +9,7 @@ export const Header: Component = () => {
             <Container padding="2em 0 0 0">
                 <HStack justifyContent="space-between">
                     <Heading size="3xl">NMS Updates</Heading>
-                    <BasicLink href="https://www.nomanssky.com/waypoint-update/" additionalClassNames="hidden-in-mobile">
+                    <BasicLink href="https://www.nomanssky.com/omega-update/" additionalClassNames="hidden-in-mobile">
                         <Box border="2px solid" borderRadius="0.5em" padding="0.5em 1em">
                             <Text color="white">Latest Patch Notes</Text>
                         </Box>
